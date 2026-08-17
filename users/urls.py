018e5b43d07baa55f8406e71e93acbbec5e1e8dc
mehedi259 <mmridul207@gmail.com>
2026-08-17 20:22:28 +0600

    feat: add UserViewSet for administrative user management via router

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,15 @@
-from django.urls import path
+from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from .views import RegisterView, UserProfileView
+from rest_framework.routers import DefaultRouter
+from .views import RegisterView, UserProfileView, UserViewSet
+
+router = DefaultRouter()
+router.register(r'users', UserViewSet, basename='user')
 
 urlpatterns = [
     path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('login/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('register/', RegisterView.as_view(), name='auth_register'),
     path('me/', UserProfileView.as_view(), name='auth_me'),
+    path('', include(router.urls)),
 ]
--- users/views.py
+++ users/views.py
@@ -16,3 +16,10 @@ class UserProfileView(generics.RetrieveAPIView):
 
     def get_object(self):
         return self.request.user
+
+from rest_framework import viewsets
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAdminUser]
